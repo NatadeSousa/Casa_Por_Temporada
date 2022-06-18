fa5471a0c98e2b49cc1c94a49b73df180686df4e
Ins1ghtful <natan345894@gmail.com>
2022-06-17 23:02:14 -0300

    Layout da tela de cadastro concluído

--- app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/LoginActivity.java
+++ app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/LoginActivity.java
@@ -2,15 +2,35 @@ package com.example.casa_por_temporada.Activity.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.casa_por_temporada.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView textCreateAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        referComponents();
+        configClicks();
+    }
+
+    private void configClicks(){
+
+        textCreateAccount.setOnClickListener(view -> {
+            startActivity(new Intent(this, CreateAccountActivity.class));
+        });
+
+    }
+    private void referComponents(){
+
+        textCreateAccount = findViewById(R.id.text_createAccount);
+
     }
 }
