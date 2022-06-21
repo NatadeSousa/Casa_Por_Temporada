5d1cf32dce102192521e44ff6db4aec198098702
Ins1ghtful <natan345894@gmail.com>
2022-06-21 09:04:53 -0300

    Layout de novo anúncio configurado

--- app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/LoginActivity.java
+++ app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/LoginActivity.java
@@ -4,24 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.casa_por_temporada.Activity.MainActivity;
+import com.example.casa_por_temporada.Helper.FirebaseHelper;
 import com.example.casa_por_temporada.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView textCreateAccount, textResetPassword,textMainToolbar;
+    private EditText editEmail, editPassword;
+    private ProgressBar progressBarLogin;
+    private Button btnLogin;
 
-
+    //Activity Life Cycles
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         referComponents();
         configClicks();
-    }
 
+    }
+    //-------------------------------------------------------------------------
+
+
+    //Setting user login to account
+    private void validateUserData(){
+
+        String email = editEmail.getText().toString();
+        String password = editPassword.getText().toString();
+
+        if(!email.isEmpty()){
+            if(!password.isEmpty()){
+
+                progressBarLogin.setVisibility(View.VISIBLE);
+                signInAccount(email,password);
+
+            }else{
+                editPassword.requestFocus();
+                editPassword.setError("Digite a sua senha");
+            }
+        }else{
+            editEmail.requestFocus();
+            editEmail.setError("Digite o seu e-mail");
+        }
+
+    }
+    private void signInAccount(String email, String password){
+
+        FirebaseHelper.getAuth().signInWithEmailAndPassword(
+                email,password
+        ).addOnCompleteListener(task -> {
+
+            if(task.isSuccessful()){
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+            }else{
+                String error = task.getException().getMessage();
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+    //-------------------------------------------------------------------------
+
+    //Setting the buttons
     private void configClicks(){
 
         textCreateAccount.setOnClickListener(view -> {
@@ -32,11 +86,24 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this,ResetPasswordActivity.class));
         });
 
+        btnLogin.setOnClickListener(view -> {
+            validateUserData();
+        });
+
     }
+    //-------------------------------------------------------------------------
+
+    //Referring components
     private void referComponents(){
 
         textCreateAccount = findViewById(R.id.text_createAccount);
         textResetPassword = findViewById(R.id.text_resetPassword);
         textMainToolbar = findViewById(R.id.text_main_toolbar);
+        progressBarLogin = findViewById(R.id.progressBarLogin);
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        btnLogin = findViewById(R.id.btn_login);
+
     }
+    //-------------------------------------------------------------------------
 }
--- app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/ResetPasswordActivity.java
+++ app/src/main/java/com/example/casa_por_temporada/Activity/Authentication/ResetPasswordActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.casa_por_temporada.Helper.FirebaseHelper;
 import com.example.casa_por_temporada.R;
 
 public class ResetPasswordActivity extends AppCompatActivity {
@@ -15,6 +22,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
     private TextView textOnToolBar;
     private Button btnResetPassword;
     private ImageButton btnGetBack;
+    private EditText editEmail;
+    private ProgressBar progressBarResetPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,51 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
     }
 
+
+    private void validateUserData(){
+
+        String email = editEmail.getText().toString();
+
+        if(!email.isEmpty()){
+
+            progressBarResetPassword.setVisibility(View.VISIBLE);
+
+            sendEmailToUser(email);
+
+        }else{
+            editEmail.requestFocus();
+            editEmail.setError("Digite o seu email");
+        }
+
+    }
+    private void sendEmailToUser(String email){
+
+        FirebaseHelper.getAuth().sendPasswordResetEmail(
+                email
+        ).addOnCompleteListener(task -> {
+
+            if(task.isSuccessful()){
+                Toast.makeText(this, "E-mail enviado com sucesso!", Toast.LENGTH_SHORT).show();
+                new Handler(Looper.getMainLooper()).postDelayed(this::goToLoginActivity,2500);
+            }else{
+                String error = task.getException().getMessage();
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+            }
+            progressBarResetPassword.setVisibility(View.GONE);
+        });
+
+    }
+    private void configClicks(){
+
+        btnGetBack.setOnClickListener(view -> {
+            startActivity(new Intent(this,LoginActivity.class));
+        });
+
+        btnResetPassword.setOnClickListener(view -> {
+            validateUserData();
+        });
+
+    }
     private void setTextOnToolBar(){
 
         textOnToolBar.setText("Recuperar senha");
@@ -35,13 +90,15 @@ public class ResetPasswordActivity extends AppCompatActivity {
         btnResetPassword = findViewById(R.id.btn_reset_password);
         textOnToolBar = findViewById(R.id.text_main_toolbar);
         btnGetBack = findViewById(R.id.btn_getback);
-    }
-    private void configClicks(){
-
-        btnGetBack.setOnClickListener(view -> {
-            startActivity(new Intent(this,LoginActivity.class));
-        });
+        editEmail = findViewById(R.id.edit_email);
+        progressBarResetPassword = findViewById(R.id.progress_bar_reset_password);
+        btnResetPassword = findViewById(R.id.btn_reset_password);
 
     }
+    public void goToLoginActivity(){
+        finish();
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+
 
 }
