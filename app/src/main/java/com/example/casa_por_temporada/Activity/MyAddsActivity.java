52e28f2cac0cb1dea597d1d601f69d96b802ca7b
Ins1ghtful <natan345894@gmail.com>
2022-06-23 23:43:17 -0300

    Activity Meus Anúncios configurada

--- app/src/main/java/com/example/casa_por_temporada/Activity/MyAddsActivity.java
+++ app/src/main/java/com/example/casa_por_temporada/Activity/MyAddsActivity.java
@@ -1,16 +1,102 @@
 package com.example.casa_por_temporada.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.casa_por_temporada.Adapter.AdapterHomes;
+import com.example.casa_por_temporada.Helper.FirebaseHelper;
+import com.example.casa_por_temporada.Model.Home;
 import com.example.casa_por_temporada.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class MyAddsActivity extends AppCompatActivity {
 
+    private List<Home> homeList = new ArrayList<>();
+    private ProgressBar progressBarMyAddsActivity;
+    private TextView textInfo;
+    private RecyclerView rvMyAdds;
+    private AdapterHomes adapterHomes;
+
+    //Activities' life cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_adds);
+        referComponents();
+        setRecyclerView();
+        recoverAddsOnDatabase();
     }
+    //--------------------------------------------------------------------------------
+
+
+    //Setting recyclerView
+    private void setRecyclerView(){
+        rvMyAdds.setLayoutManager(new LinearLayoutManager(this));
+        rvMyAdds.setHasFixedSize(true);
+        adapterHomes = new AdapterHomes(homeList);
+        rvMyAdds.setAdapter(adapterHomes);
+    }
+    //--------------------------------------------------------------------------------
+
+    //Recovering adds from Database
+    private void recoverAddsOnDatabase(){
+        DatabaseReference databaseReference = FirebaseHelper.getDatabaseReference()
+                .child("adds")
+                .child(FirebaseHelper.getUserIdOnDatabase());
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        homeList.clear();
+                        textInfo.setVisibility(View.GONE);
+                        for(DataSnapshot snap : snapshot.getChildren()){
+                            Home home = snap.getValue(Home.class);
+                            homeList.add(home);
+                        }
+                    }else{
+                        textInfo.setText("Nenhum anúncio registrado");
+                    }
+
+                    progressBarMyAddsActivity.setVisibility(View.GONE);
+                    Collections.reverse(homeList);
+                    adapterHomes.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(MyAddsActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    }
+    //--------------------------------------------------------------------------------
+
+    //Referring components
+    private void referComponents(){
+
+        textInfo = findViewById(R.id.text_info);
+        progressBarMyAddsActivity = findViewById(R.id.progressBarMyAddsActivity);
+        rvMyAdds = findViewById(R.id.rv_my_adds);
+
+    }
+    //--------------------------------------------------------------------------------
+
 }
