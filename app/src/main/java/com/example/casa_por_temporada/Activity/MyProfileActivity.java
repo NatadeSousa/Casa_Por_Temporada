8a23e8b9ee5a7790927cfdd9e7c8388f70d5ef4a
Ins1ghtful <natan345894@gmail.com>
2022-06-29 16:27:59 -0300

    1.0

--- app/src/main/java/com/example/casa_por_temporada/Activity/MyProfileActivity.java
+++ app/src/main/java/com/example/casa_por_temporada/Activity/MyProfileActivity.java
@@ -35,12 +35,10 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
-
         referComponents();
         setTextOnToolbar();
         recoverData();
         setClicks();
-
     }
     //-----------------------------------------------------------------------------------
 
